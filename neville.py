5e5b38997dc2ce667cfe3e0070e588596b8ab1c1
hatemBT <hatemtayeb2@gmail.com>
2018-11-15 19:45:12 +0100

    hello

--- neville.py
+++ neville.py
@@ -3,10 +3,10 @@ from numpy import *
 def Neville(x, xdata, ydata):
     """return p[xdata](x)"""
     xdata = x - xdata
-    print xdata
+    print(xdata)
     n = len(xdata)
     for i in range(1,n):
-        num = xdata[:n-i]*ydata[1:n-i+1] - xdata[1:n-i+1]*ydata[:n-i]       
+        num = xdata[:n-i]*ydata[1:n-i+1] - xdata[1:n-i+1]*ydata[:n-i]
         den = xdata[:n-i] - xdata[i:n]
         ##ydata[:n-i] = num/den
         ydata[:n-i] = (xdata[:n-i]*ydata[1:n-i+1] - xdata[1:n-i+1]*ydata[:n-i])/(xdata[:n-i] - xdata[i:n])
@@ -15,4 +15,4 @@ def Neville(x, xdata, ydata):
 
 
 
-print Neville(0,array([1,2,3]),array([1,2,1]))
+print(Neville(0,array([1,2,3]),array([1,2,1]))
