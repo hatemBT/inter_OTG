289b5d6fea3bc9c7185b238eefe2e30556ecfaa2
hatemBT <hatemtayeb2@gmail.com>
2018-11-16 13:06:42 +0100

    ok

--- neville.py
+++ neville.py
@@ -8,12 +8,13 @@
 from scipy.interpolate import lagrange,interp1d
 from numpy.polynomial.polynomial import Polynomial
 import sys
-import time
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QGroupBox, QFormLayout, QPushButton, QLabel, QLineEdit, \
-    QVBoxLayout, QTableWidget, QComboBox, QTableWidgetItem
+    QVBoxLayout, QTableWidget, QComboBox, QTableWidgetItem, QSizePolicy
 
 
 class inter_OTG(QWidget):
@@ -24,9 +25,12 @@ class inter_OTG(QWidget):
         self.x = []
         self.y = []
         super(inter_OTG, self).__init__()
-        self.setGeometry(100, 200, 287, 400)
+        self.setGeometry(100, 200, 815, 450)
         self.setWindowTitle("Easy_interpolation")
         self.grid = QGridLayout()
+        self.grid2 = QGridLayout()
+        self.allGB = QGroupBox("settings")
+        self.allGB.setLayout(self.grid2)
         # -------------------------------------------------
         self.combo = QComboBox()
         itm= ["lagrange","neville","newton"]
@@ -40,14 +44,15 @@ class inter_OTG(QWidget):
         self.clear = QPushButton("clear")
 
         # -------------------------------------------------
-        self.qb1 = QGroupBox("View DATA")
+        self.qb1 = QGroupBox("Load DATA") #--->
+        self.plotfig = QGroupBox("interpolate")
         self.qV = QVBoxLayout()
 
         self.table = QTableWidget()
         self.table.setRowCount(10)
         self.table.setColumnCount(2)
         self.table.setHorizontalHeaderLabels(["Xi", "Yi"])
-
+        self.m = PlotCanvas(self, width=5, height=4)
         self.load = QPushButton("Load Data")
         self.interP = QPushButton("interpolate points")
         # -------------------------------------------------
@@ -60,21 +65,35 @@ class inter_OTG(QWidget):
         self.qf.addRow(self.start, self.clear)
         self.qb.setLayout(self.qf)
 
-        self.grid.addWidget(self.qb, 0, 0)
-        self.grid.addWidget(self.qb1, 1, 0)
+        self.qV2 = QVBoxLayout() #---->
+        self.qV2.addWidget(self.m)
+        self.plotfig.setLayout(self.qV2)
+
+
+        self.grid2.addWidget(self.qb, 0, 0)
+        self.grid2.addWidget(self.qb1, 1, 0)
+        self.grid.addWidget(self.allGB,0,0)
+        self.grid.addWidget(self.plotfig,0,1)
         self.setLayout(self.grid)
         # -------------------------------------------------
 
         self.start.clicked.connect(lambda : get_points(self))
         self.load.clicked.connect(lambda : load_data(self))
-        self.interP.clicked.connect(lambda :interpolate(self))
-        self.clear.clicked.connect(lambda : clear_data())
+        self.interP.clicked.connect(lambda :self.m.interpolate())
+        self.clear.clicked.connect(lambda : clear_data(self))
 
 
 
-def clear_data():
+def clear_data(self):
     os.remove("xpoints.txt")
     os.remove("ypoints.txt")
+    for i in range(self.table.rowCount()):
+        for j in range(self.table.columnCount()):
+            self.table.setItem(i, j, QTableWidgetItem("0.0"))
+    global xi
+    global yi
+    xi=[]
+    yi=[]
 
 def get_points(self):
     x = []
@@ -132,34 +151,59 @@ def load_data(self):
                 pass
 
 
+
+
     print(xi)
     print(yi)
 
 
-def interpolate(self):
-    x= np.array(xi)
-    y= np.array(yi)
-
-    x0 = float(self.x0F.text())
-    fit = np.polyfit(x,y,x0)
-    xx = np.linspace(min(x), max(x))
-    yy = np.polyval(fit, xx)
-
-    plt.plot(xx, yy, '-', x, y, 'ro')
-    plt.axis([min(xx) , max(xx) , min(yy) , max(yy) ]);
-
-    # poly = lagrange(x, y)
-    # f = Polynomial(poly).coef
-    # print(f)
-    # # calculate new x's and y's
-    #
-    # t = np.linspace(0, 20, 100)
-
-
-    plt.show()
 
 
 
+class PlotCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        #self.interpolate()
+
+    # def plot(self):
+    #     data = [np.random.random() for i in range(25)]
+    #     ax = self.figure.add_subplot(111)
+    #     ax.plot(data, 'r-')
+    #     ax.set_title('Interpolation ')
+    #     self.draw()
+
+    def interpolate(self):
+            x = np.array(xi)
+            y = np.array(yi)
+
+            #x0 = float(self.x0F.text())
+            fit = np.polyfit(x, y, len(x) - 1)
+            xx = np.linspace(min(x), max(x))
+            yy = np.polyval(fit, xx)
+            ax = self.figure.add_subplot(111)
+            ax.plot(xx, yy, '-', x, y, 'ro')
+            ax.axis([min(xx), max(xx), min(yy), max(yy)]);
+
+            # poly = lagrange(x, y)
+            # f = Polynomial(poly).coef
+            # print(f)
+            # # calculate new x's and y's
+            #
+            # t = np.linspace(0, 20, 100)
+
+            self.draw()
+            x=[]
+            y=[]
 
 
 
--- test.py
+++ test.py
@@ -1,27 +1,80 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.interpolate import lagrange
+# from numpy.polynomial.polynomial import Polynomial
+#
+# # get x and y vectors
+# #
+# # x=[1, 4, 6, 8]
+# # y=[7, 6, 8, 4]
+# #
+# # #x= [np.round(i)for i in x1]
+# # #y= [np.round(i)for i in y1]
+# # print(x)
+# # print(y)
+# #
+# # # calculate polynomial
+# # poly = lagrange(x, y)
+# # f=Polynomial(poly).coef
+# # print(f)
+# # # calculate new x's and y's
+# #
+# # t=np.linspace(0,10,10)
+# #
+# # plt.plot(x,y,'o', np.polyval(f,t))
+# # plt.show()
+#
+#
+#
+#
+# """
+# Show how to modify the coordinate formatter to report the image "z"
+# value of the nearest pixel given x and y
+# """
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+#
+# X = 10*np.random.rand(5,3)
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.imshow(X, cmap=cm.jet, interpolation='nearest')
+#
+# numrows, numcols = X.shape
+# def format_coord(x, y):
+#     col = int(x+0.5)
+#     row = int(y+0.5)
+#     if col>=0 and col<numcols and row>=0 and row<numrows:
+#         z = X[row,col]
+#         return 'x=%1.4f, y=%1.4f, z=%1.4f'%(x, y, z)
+#     else:
+#         return 'x=%1.4f, y=%1.4f'%(x, y)
+#
+# ax.format_coord = format_coord
+# plt.show()
+#
+#
+
+
+from matplotlib.widgets import Cursor
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import lagrange
-from numpy.polynomial.polynomial import Polynomial
 
-# get x and y vectors
 
-x=[1, 4, 6, 8]
-y=[7, 6, 8, 4]
+# Fixing random state for reproducibility
+np.random.seed(19680801)
 
-#x= [np.round(i)for i in x1]
-#y= [np.round(i)for i in y1]
-print(x)
-print(y)
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, facecolor='#FFFFCC')
 
-# calculate polynomial
-poly = lagrange(x, y)
-f=Polynomial(poly).coef
-print(f)
-# calculate new x's and y's
+x, y = 4*(np.random.rand(2, 100) - .5)
+ax.plot(x, y, 'o')
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
 
-t=np.linspace(0,10,10)
+# Set useblit=True on most backends for enhanced performance.
+cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
 
-plt.plot(x,y,'o', np.polyval(f,t))
 plt.show()
-
 
